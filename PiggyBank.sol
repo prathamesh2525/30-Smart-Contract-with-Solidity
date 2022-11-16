b8ebcd61b4c569a247ee1dd88cf72e027c1bd406
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2022-11-16 23:16:43 +0530

    Update PiggyBank.sol

--- PiggyBank.sol
+++ PiggyBank.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Piggy Bank
+// -  User can deposit in the wallet 
+// - Owner can Withdraw the amount
+// - The Contract should be destroyed after withdrawal
+
 contract PiggyBank{
     address payable public owner;
 
@@ -19,7 +24,11 @@ contract PiggyBank{
     function withdraw() public onlyOwner {
         owner.transfer(address(this).balance);
         emit Withdraw(msg.sender,address(this).balance);
-        selfdestruct(payable(owner));  // function destroys smart contract for address 
+        selfdestruct(payable(owner)); 
+        /// selfdestruct can be used to destory the contract 
+        /// and send all the eth to the address mentioned  ,it deleted the ABI associated with it , therefore no function call work 
+        /// there can be an attack too using selfdestruct where user can force send ether from attack contract
+        /// to the target contract , destructing this attack contract , more on that in upcoming thread
     }  
 
     receive() payable external {
